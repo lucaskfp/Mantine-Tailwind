2ead4e47a9648501dceac1eebb93c596162367dd
Lucas Kennedy <lukakfp@gmail.com>
2023-11-28 21:10:42 -0300

    chore: First commit

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import "@mantine/core/styles.css";
+import "./tailwind.css";
+
+import { App } from "./App.tsx";
+import { MantineProvider } from "@mantine/core";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <MantineProvider defaultColorScheme="auto">
+      <App />
+    </MantineProvider>
+  </React.StrictMode>
+);
